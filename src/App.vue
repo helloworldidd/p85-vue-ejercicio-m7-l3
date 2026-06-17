9e619e80f4793f8c74694afb42ca9982a23f8ee2
helloworldidd <helloworld@idd.cl>
2026-06-17 14:06:27 -0400

    base del ejercicio

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+
+<nav>
+  <router-link class="btn btn-primary m-2" to="/">Login</router-link>
+  <router-link class="btn btn-primary m-2" to="/dashboard">Dashboard</router-link>
+</nav>
+
+<router-view></router-view>
+</template>
+
+<style scoped>
+
+</style>
